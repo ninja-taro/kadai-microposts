3a3e5417eb17f5dfe7d012a1d762a8e85a095f77
ninja.taro <ninja.taro@hotmail.com>
2019-10-06 10:46:16 +0000

    favorite2

--- resources/views/favorite/favorite_button.blade.php
+++ resources/views/favorite/favorite_button.blade.php
@@ -1,4 +1,4 @@
-@if(Auth::id() != $micropost->user_id)
+<!-- @if(Auth::id() != $micropost->user_id) -->
     @if (Auth::user()->is_favoriting($micropost->id))
         {!! Form::open(['route' => ['favorites.unfavorite', $micropost->id], 'method' => 'delete']) !!}
             {!! Form::submit('Unfavorite', ['class' => "btn btn-dark"]) !!}
@@ -8,4 +8,4 @@
             {!! Form::submit('Favorite', ['class' => "btn btn-success"]) !!}
         {!! Form::close() !!}
     @endif
-@endif
+<!-- @endif -->
--- resources/views/microposts/microposts.blade.php
+++ resources/views/microposts/microposts.blade.php
@@ -15,8 +15,39 @@
                             {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-sm']) !!}
                         {!! Form::close() !!}
                     @endif
-                    @include('favorite.favorite_button', ['user' => $user])
+                    
                 </div>
+                <div>
+                    @include('favorite.favorite_button')
+                </div>
+                
+            </div>
+        </li>
+    @endforeach
+</ul>
+{{ $microposts->links('pagination::bootstrap-4') }}<ul class="list-unstyled">
+    @foreach ($microposts as $micropost)
+        <li class="media mb-3">
+            <img class="mr-2 rounded" src="{{ Gravatar::src($micropost->user->email, 50) }}" alt="">
+            <div class="media-body">
+                <div>
+                    {!! link_to_route('users.show', $micropost->user->name, ['id' => $micropost->user->id]) !!} <span class="text-muted">posted at {{ $micropost->created_at }}</span>
+                </div>
+                <div>
+                    <p class="mb-0">{!! nl2br(e($micropost->content)) !!}</p>
+                </div>
+                <div>
+                    @if (Auth::id() == $micropost->user_id)
+                        {!! Form::open(['route' => ['microposts.destroy', $micropost->id], 'method' => 'delete']) !!}
+                            {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-sm']) !!}
+                        {!! Form::close() !!}
+                    @endif
+                    
+                </div>
+                <div>
+                    @include('favorite.favorite_button')
+                </div>
+                
             </div>
         </li>
     @endforeach
